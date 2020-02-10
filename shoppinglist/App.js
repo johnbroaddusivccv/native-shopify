5c89e4bffe0367a039405ad43744a5e7b9b66f7f
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2020-02-10 07:12:29 -0600

    pushing up with error can't find -- yoganode

--- shoppinglist/App.js
+++ shoppinglist/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>John Morgan Broaddus IV</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-        style={styles.img}
+      <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} />}
       />
     </View>
   );
@@ -16,14 +26,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {color: 'darkslateblue', fontSize: 30},
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
   },
 });
 export default App;
